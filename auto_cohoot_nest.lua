b57056f6442a6c2bf249e50f6c0dbc4cfabaa1fb
fadillzzz <fadillzzz@gmail.com>
2022-08-25 16:14:53 +0700

    feat(nest): Auto loot both nests

--- auto_cohoot_nest.lua
+++ auto_cohoot_nest.lua
@@ -6,39 +6,42 @@ local settings = {
 	maxStock = 5;
 }
 
-local elgadoMap = {[6]=true}
-local kamuraMap = {[0]=true, [1]=true, [2]=true, [3]=true, [4]=true, [7]=true}
+local elgadoMap = {[6]=true, [7]=true}
+local kamuraMap = {[0]=true, [1]=true, [2]=true, [3]=true, [4]=true}
 
 function autoPickNest(retval)
 	if not settings.enable then
 		return
 	end
-	
+
 	local villageAreaManager = sdk.get_managed_singleton("snow.VillageAreaManager")
-	
+
 	if not villageAreaManager then
 		return
 	end
-	
+
 	local villageNum = villageAreaManager:call("get__CurrentAreaNo")
-	
+
 	local owlNestManagerSingleton = sdk.get_managed_singleton("snow.progress.ProgressOwlNestManager")
 	local progressOwlNestSaveData = owlNestManagerSingleton:get_SaveData()
 
 	if not owlNestManagerSingleton or not progressOwlNestSaveData then
 		return
 	end
-	
-	local owlNestCount = 0
-	if elgadoMap[villageNum] then
-		owlNestCount = progressOwlNestSaveData:get_field("_StackCount2")
-	elseif kamuraMap[villageNum] then
-		owlNestCount = progressOwlNestSaveData:get_field("_StackCount")
-	end
-	
-	if owlNestCount >= settings.maxStock and owlNestManagerSingleton then
+
+	local kamuraCount = progressOwlNestSaveData:get_field("_StackCount")
+	local elgadoCount = progressOwlNestSaveData:get_field("_StackCount2")
+
+	if kamuraCount >= settings.maxStock then
+		villageAreaManager:call("set__CurrentAreaNo", 2)
 		owlNestManagerSingleton:supply()
 	end
+
+	if elgadoCount >= settings.maxStock then
+		villageAreaManager:call("set__CurrentAreaNo", 6)
+		owlNestManagerSingleton:supply()
+	end
+
 end
 
 local function SaveSettings()
@@ -58,12 +61,12 @@ end
 re.on_draw_ui(function()
 	local changed = false;
 
-    if imgui.tree_node("Auto cohoot nest") then
+	if imgui.tree_node("Auto cohoot nest") then
 
 		changed, settings.enable = imgui.checkbox("Enabled", settings.enable);
 		changed, settings.maxStock = imgui.slider_int("Maximum stock", settings.maxStock, 1, 5);
 		imgui.tree_pop()
-    end
+	end
 end)
 
 re.on_config_save(function()
